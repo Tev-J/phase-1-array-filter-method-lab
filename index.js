057a0d2b8429f131f07209cf3b9cad562e317ad4
Tev-J <tj_delph@yahoo.com>
2023-04-08 00:25:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(driverList, driver) {
+  return driverList.filter(
+    (name) => name.toUpperCase() === driver.toUpperCase()
+  );
+}
+
+function fuzzyMatch(dList, letters) {
+  return dList.filter((driver) => driver.startsWith(letters));
+}
+
+function matchName(dList, driverName) {
+  return dList.filter((obj) => obj.name === driverName);
+}
